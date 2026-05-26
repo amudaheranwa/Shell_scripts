04aa2cd1fdb52cb238160fa10abfab6f73f5e886
Alvin <a.mudaheran@alustudent.com>
2026-05-26 10:36:54 +0200

    Shell scripts

---
+++ Script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Directory_Name="assignment_dir"
+Path="/home/amudaheran/Shell_scripts"
+if [ -d "$Path" ];
+then echo "Directory with a Name '$Directory_Name' exists in the location"
+else echo "No such Directory"
+fi
+
