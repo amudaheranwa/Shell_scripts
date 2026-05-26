357e1210530ec73922a01ed145a89a1312c07d2c
amudaheranwa <a.mudaheran@alustudent.com>
2026-05-26 01:40:15 -0700

    Scripts for today

---
+++ Scripts.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+Directory_Name = "assignment_dir"
+Path = "/home/amudaheran/Shell_scripts"
+if [-d "$Path"]; then
+echo "Directory with a Name '$Directory_Name' exits in the Location"
+else echo "No such a Directory"
+fi
